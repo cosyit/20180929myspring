9cebe2c85e65eb6292d8c7ac04d18dc2f28341c4
MuMu <610495444@qq.com>
2018-09-29 02:13:11 +0800

    第一天学习spring

---
+++ src/main/java/com/cosyit/myspring/第1天/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.cosyit.myspring.第1天;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class HelloWorld {
+
+    private static Log log = LogFactory.getLog(HelloWorld.class);
+
+    private String name;
+
+    public HelloWorld() {
+        log.info("spring先加载xml配置，再调用无参构造器正在构造bean，如果没有无参构造器，spring无法工作");
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void sayHelloWorld(){
+        System.out.println(name +" say :Hello World!");
+    }
+}
---
+++ src/main/java/com/cosyit/myspring/第1天/Main.java
@@ -0,0 +1,33 @@
+package com.cosyit.myspring.第1天;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    private final static Log log = LogFactory.getLog(Main.class);
+
+    public static void main(String[] args) {
+        // todo : 使用spring之后第一步和第2步交给spring去做。
+        //第一步：创建对象
+        HelloWorld helloWorld =  new HelloWorld();
+        //第二步：给对象赋值
+        helloWorld.setName("david teacher");
+
+        helloWorld.sayHelloWorld();
+
+        /**
+         * -----------------------------当我们使用spring之后的做法--------------------------------------
+         */
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:第一天helloworldapplicationContext.xml");
+
+        System.out.println(ctx);
+
+        HelloWorld helloWorld1 = ctx.getBean("helloWorld",HelloWorld.class);
+        helloWorld1.sayHelloWorld();
+        ((ClassPathXmlApplicationContext) ctx).close();
+    }
+}
