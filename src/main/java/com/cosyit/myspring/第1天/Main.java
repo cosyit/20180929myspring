178381195a8b09182e5ace7c89c91c248e191e80
MuMu <610495444@qq.com>
2018-10-03 04:11:18 +0800

    重新复习了一下helloworld,并整理了一下此demo 运行顺序。

--- src/main/java/com/cosyit/myspring/第1天/Main.java
+++ src/main/java/com/cosyit/myspring/第1天/Main.java
@@ -11,22 +11,25 @@ public class Main {
 
     public static void main(String[] args) {
         // todo : 使用spring之后第一步和第2步交给spring去做。
-        //第一步：创建对象
-        HelloWorld helloWorld =  new HelloWorld();
-        //第二步：给对象赋值
-        helloWorld.setName("david teacher");
-
-        helloWorld.sayHelloWorld();
+//        //第一步：创建对象
+//        HelloWorld helloWorld =  new HelloWorld();
+//        //第二步：给对象赋值
+//        helloWorld.setName("david teacher");
+//
+//        helloWorld.sayHelloWorld();
 
         /**
          * -----------------------------当我们使用spring之后的做法--------------------------------------
          */
-
+        // 001. 先加载applicaitonContext.xml文件。
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:第一天helloworldapplicationContext.xml");
 
-        System.out.println(ctx);
-
+        // 002. 调用无参构造函数，构造bean.
         HelloWorld helloWorld1 = ctx.getBean("helloWorld",HelloWorld.class);
+
+        //003. 调用setProperty 进行初始化。出我们需要得对象。
+
+        //004. 对象.方法();
         helloWorld1.sayHelloWorld();
         ((ClassPathXmlApplicationContext) ctx).close();
     }
